19a6af81fe3cd59d73b46d0794ed2a26b27f2349
Nurlan Kalybekov <carjapancar@gmail.com>
2024-04-08 22:00:47 +0900

    Create custom TimeView

---
+++ TimeRange/TimeView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeView.swift
+//  TimeRange
+//
+//  Created by Nurlan on 2024/04/08.
+//
+
+import UIKit
+
+class TimeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
