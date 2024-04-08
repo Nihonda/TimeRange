fbad19cb87770829a614f8521a714271ab7cc192
Nurlan Kalybekov <carjapancar@gmail.com>
2024-04-08 23:26:15 +0900

    Add elements to main controller

--- TimeRange/TimeRangeViewController.swift
+++ TimeRange/TimeRangeViewController.swift
@@ -7,13 +7,87 @@
 
 import UIKit
 
-class TimeRangeViewController: UIViewController {
+struct TimeScreenLayout {
+    struct Paddings {
+        static let left: CGFloat = 50
+        static let right: CGFloat = 50
+        static let shiftTop: CGFloat = 100
+        static let shiftBottom: CGFloat = 50
+    }
+}
 
+class TimeRangeViewController: UIViewController {
+    typealias Padding = TimeScreenLayout.Paddings
+    
+    // Elements initialization
+    private lazy var startTimeView: TimeView = {
+        let view = TimeView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setCaption(with: "Start Time")
+        return view
+    }()
+    
+    private lazy var endTimeView: TimeView = {
+        let view = TimeView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setCaption(with: "End Time")
+        return view
+    }()
+    
+    private lazy var checkTimeView: TimeView = {
+        let view = TimeView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setCaption(with: "Check Range")
+        return view
+    }()
+    
+    private lazy var runButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Run", for: .normal)
+        
+        // layer
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
     }
 
-
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        // Add elements
+        view.addSubview(startTimeView)
+        view.addSubview(endTimeView)
+        view.addSubview(checkTimeView)
+        view.addSubview(runButton)
+        
+        // Set constraints
+        NSLayoutConstraint.activate([
+            // Start time view
+            startTimeView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Padding.left),
+            startTimeView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -Padding.shiftTop),
+            
+            // End time view
+            endTimeView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Padding.right),
+            endTimeView.topAnchor.constraint(equalTo: startTimeView.topAnchor),
+            
+            // Check time view
+            checkTimeView.topAnchor.constraint(equalTo: startTimeView.bottomAnchor, constant: Padding.shiftBottom),
+            checkTimeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            // Run button
+            runButton.topAnchor.constraint(equalTo: checkTimeView.bottomAnchor, constant: Padding.shiftBottom),
+            runButton.widthAnchor.constraint(equalTo: checkTimeView.widthAnchor),
+            runButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
