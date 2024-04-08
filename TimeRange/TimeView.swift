f69028268239c667e61c4df63a95cebfece10fe5
Nurlan Kalybekov <carjapancar@gmail.com>
2024-04-08 22:17:45 +0900

    Add subviews label and time picker to custom TimeView

--- TimeRange/TimeView.swift
+++ TimeRange/TimeView.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class TimeView: UIView {
+    lazy var captionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var timePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .time
+        return picker
+    }()
+    
+    lazy var timeTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.inputView = timePicker
+        return textField
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(captionLabel)
+        addSubview(timeTextField)
     }
 }
