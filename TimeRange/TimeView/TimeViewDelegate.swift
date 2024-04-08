e74bd5f9dbbb20918073dde18a93b135a73492ab
Nurlan Kalybekov <carjapancar@gmail.com>
2024-04-08 23:59:47 +0900

    Create TimeViewDelegate to pass data from custom TimeView to controller

---
+++ TimeRange/TimeView/TimeViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  TimeViewDelegate.swift
+//  TimeRange
+//
+//  Created by Nurlan on 2024/04/08.
+//
+
+import Foundation
+
+// This delegate class sends data from TimeView to TimeViewController
+protocol TimeViewDelegate: NSObject {
+    func onTextChanged(_ text: String)
+    func onClickRun()
+}
